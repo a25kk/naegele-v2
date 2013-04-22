d0bb5a5c183fd033e8379dc25c17555247ac9aa6
Serge Davidov <sd@kreativkombinat.de>
2013-04-22 15:38:13 +0200

    packages added

---
+++ src/vesna.portlets.collection/vesna/portlets/collection/__init__.py
@@ -0,0 +1,5 @@
+from zope.i18nmessageid import MessageFactory
+collection_portletMessageFactory = MessageFactory('vesna.portlets.collection')
+
+def initialize(context):
+    """Initializer called when used as a Zope 2 product."""
